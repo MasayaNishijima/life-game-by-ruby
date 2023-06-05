67e421e78c02eff79d27578956d4a7d2dee6838c
MasayaNishijima <25636356+MasayaNishijima@users.noreply.github.com>
2023-06-05 21:10:11 +0900

    test: セルの生死についてテストを作成

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,4 +1,25 @@
 require 'cell'
 
 RSpec.describe Cell do
+  describe 'セルの生死について' do
+    context 'セル死んでいるとき' do
+      let(:cell) { Cell.new(:death) }
+
+      it 'alive?がfalseになること' do
+        expect(cell.alive?).to be false
+      end
+    end
+
+    context 'セルが生きているとき' do
+      let(:cell) { Cell.new(:alive) }
+
+      it 'alive?がtrueになること' do
+        expect(cell.alive?).to be true
+      end
+    end
+
+    it '生死を指定していないセルが生成できないこと' do
+      expect(Cell.new).to raise_error ArgumentError
+    end
+  end
 end
